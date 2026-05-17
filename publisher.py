c5ec9d7bee7cb598aa6f5efc133aeae95f313605
SandraKTI2025 <sandra.savinski@tktk.ee>
2026-05-18 01:41:25 +0300

    Create publisher.py

---
+++ publisher.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+def get_games_by_publisher(publisher_name):
+    with sqlite3.connect("db/TopGamesDB.db") as connection:
+        cursor = connection.cursor()
+
+        cursor.execute("""
+            SELECT Game.name, Publisher.name
+            FROM Game
+            JOIN GamePublisher ON Game.id = GamePublisher.gameId
+            JOIN Publisher ON Publisher.id = GamePublisher.publisherId
+            WHERE LOWER(Publisher.name) = LOWER(?)
+        """, (publisher_name,))
+        
+        return cursor.fetchall()
+
+
+games = get_games_by_publisher("Nintendo")
+
+for game in games:
+    print(f"{game[0]}, {game[1]}")
